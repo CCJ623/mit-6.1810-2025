ffe0475f0144523f6485d79f309ac2eaf8f5ab20
Jackson <1974617416@qq.com>
2026-03-09 17:04:37 +0800

    implement basic buddy memory allocation system

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -8,6 +8,12 @@
 #include "spinlock.h"
 #include "riscv.h"
 #include "defs.h"
+#include <cstddef>
+
+/*
+4k 8k 16k 32k 64k 128k 256k 512k 1m 2m
+*/
+#define BUDDY_MAX_ORDER 10
 
 void freerange(void *pa_start, void *pa_end);
 
@@ -20,22 +26,142 @@ struct run {
 
 struct {
   struct spinlock lock;
-  struct run *freelist;
+  struct run *freelist[BUDDY_MAX_ORDER];
+  struct run *usedlist[BUDDY_MAX_ORDER];
 } kmem;
 
-void
-kinit()
-{
-  initlock(&kmem.lock, "kmem");
-  freerange(end, (void*)PHYSTOP);
+inline uint64 getBuddySize(uint order) { return (uint64)PGSIZE << order; }
+
+inline uint64 getBuddy(uint64 address, uint order) {
+  return address ^ getBuddySize(order);
 }
 
-void
-freerange(void *pa_start, void *pa_end)
-{
+void buddyAdd(struct run *r, struct run *list) {
+  if (list == 0 || r < list) {
+    r->next = 0;
+    list = r;
+  } else {
+    struct run *previous = list, *next = previous->next;
+    for (; next != 0 && next < r; previous = next, next = next->next)
+      ;
+    previous->next = r;
+    r->next = next;
+  }
+}
+
+void buddyRemove(const struct run *r, struct run *list) {
+  if (list == 0)
+    return;
+
+  if (list == r) {
+    list = r->next;
+  }
+
+  struct run *previous = list, *next = previous->next;
+  while (1) {
+    if (next == r)
+      previous->next = next->next;
+    else if (next == 0)
+      return;
+
+    previous = next;
+    next = previous->next;
+  }
+}
+
+// if r exist in list return 1, otherwise 0
+int buddyExist(const struct run *r, const struct run *list) {
+  while (list != 0) {
+    if (list == r)
+      return 1;
+    else
+      list = list->next;
+  }
+  return 0;
+}
+
+void buddySplit(uint order) {
+  if (kmem.freelist[order] == 0) {
+    buddySplit(order + 1);
+    if (kmem.freelist[order] == 0)
+      return;
+  }
+
+  struct run *r = kmem.freelist[order];
+  buddyRemove(r, kmem.freelist[order]);
+  buddyAdd(r, kmem.freelist[order - 1]);
+  buddyAdd((struct run *)getBuddy((uint64)r, order - 1),
+           kmem.freelist[order - 1]);
+}
+
+void buddyCoalesce(struct run *r, uint order) {
+
+  buddyRemove(r, kmem.usedlist[order]);
+  // search buddy at every order except max order
+  while (order < BUDDY_MAX_ORDER - 1) {
+    struct run *buddy = (struct run *)getBuddy((uint64)r, order);
+    if (buddyExist(buddy, kmem.freelist[order]) == 0)
+      break;
+    buddyRemove(buddy, kmem.freelist[order]);
+    // make sure r is the start address
+    if (r > buddy)
+      r = buddy;
+    ++order;
+  }
+  buddyAdd(r, kmem.freelist[order]);
+}
+
+void *buddyAlloc(uint npages) {
+  static const uint mask = 1 << (sizeof(npages) * 8 - 1);
+  uint order = sizeof(npages) * 8;
+  // get min order to store npages
+  while (1) {
+    if (npages & mask)
+      break;
+
+    --order;
+    npages <<= 1;
+  }
+
+  acquire(&kmem.lock);
+  if (kmem.freelist[order] == 0x0) {
+    buddySplit(order + 1);
+    if (kmem.freelist[order] == 0x0)
+      return 0;
+  }
+
+  struct run *r = kmem.freelist[order];
+  buddyRemove(r, kmem.freelist[order]);
+  buddyAdd(r, kmem.usedlist[order]);
+  release(&kmem.lock);
+
+  // fill with junk
+  memset((char *)r, 5, getBuddySize(order));
+  return (void *)r;
+}
+
+void buddyFree(void *pa) {
+  acquire(&kmem.lock);
+
+  for (uint order = 0; order < BUDDY_MAX_ORDER; ++order) {
+    if (buddyExist((struct run *)(pa), kmem.usedlist[order])) {
+      buddyCoalesce((struct run *)(pa), order);
+      break;
+    }
+  }
+
+  release(&kmem.lock);
+}
+
+void kinit() {
+  initlock(&kmem.lock, "kmem");
+  freerange(end, (void *)PHYSTOP);
+}
+
+void freerange(void *pa_start, void *pa_end) {
   char *p;
-  p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  p = (char *)PGROUNDUP((uint64)pa_start);
+  for (; p + PGSIZE <= (char *)pa_end; p += PGSIZE)
     kfree(p);
 }
 
@@ -43,18 +169,16 @@ freerange(void *pa_start, void *pa_end)
 // which normally should have been returned by a
 // call to kalloc().  (The exception is when
 // initializing the allocator; see kinit above.)
-void
-kfree(void *pa)
-{
+void kfree(void *pa) {
   struct run *r;
 
-  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+  if (((uint64)pa % PGSIZE) != 0 || (char *)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
 
-  r = (struct run*)pa;
+  r = (struct run *)pa;
 
   acquire(&kmem.lock);
   r->next = kmem.freelist;
@@ -65,20 +189,18 @@ kfree(void *pa)
 // Allocate one 4096-byte page of physical memory.
 // Returns a pointer that the kernel can use.
 // Returns 0 if the memory cannot be allocated.
-void *
-kalloc(void)
-{
+void *kalloc(void) {
   struct run *r;
 
   acquire(&kmem.lock);
   r = kmem.freelist;
-  if(r)
+  if (r)
     kmem.freelist = r->next;
   release(&kmem.lock);
 
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
-  return (void*)r;
+  if (r)
+    memset((char *)r, 5, PGSIZE); // fill with junk
+  return (void *)r;
 }
 
 /*
@@ -97,7 +219,7 @@ void demoteSuperPage(void *pa) {
 // call to superalloc().  (The exception is when
 // initializing the allocator; see kinit above.)
 void superFree(void *pa) {
-  struct run *r;
+  // struct run *r;
 
   if (((uint64)pa % SUPERPGSIZE) != 0 || (char *)pa < end ||
       (uint64)pa >= PHYSTOP)
