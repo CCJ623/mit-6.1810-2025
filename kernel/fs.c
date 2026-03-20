4e4888d0e35b3748194c4119cce235938eb90a6f
Jackson <1974617416@qq.com>
2026-03-20 11:49:39 +0800

    Modify bmap() to support large files by adding double indirect block.

--- kernel/fs.c
+++ kernel/fs.c
@@ -421,17 +421,57 @@ bmap(struct inode *ip, uint bn)
 
   if(bn < NINDIRECT){
     // Load indirect block, allocating if necessary.
-    if((addr = ip->addrs[NDIRECT]) == 0){
+    if ((addr = ip->addrs[INDIRECT_INDEX]) == 0) {
       addr = balloc(ip->dev);
       if(addr == 0)
         return 0;
-      ip->addrs[NDIRECT] = addr;
+      ip->addrs[INDIRECT_INDEX] = addr;
     }
     bp = bread(ip->dev, addr);
     a = (uint*)bp->data;
     if((addr = a[bn]) == 0){
       addr = balloc(ip->dev);
       if(addr){
+        a[bn] = addr;
+        log_write(bp);
+      }
+    }
+    brelse(bp);
+    return addr;
+  }
+  bn -= NINDIRECT;
+
+  if (bn < N_DOUBLE_INDIRECT) {
+    // Load double indirect block, allocating if necessary.
+    if ((addr = ip->addrs[DOUBLE_INDIRECT_INDEX]) == 0) {
+      addr = balloc(ip->dev);
+      if (addr == 0)
+        return 0;
+      ip->addrs[DOUBLE_INDIRECT_INDEX] = addr;
+    }
+
+    // read double indirect block
+    bp = bread(ip->dev, addr);
+    a = (uint *)bp->data;
+    if ((addr = a[bn / NINDIRECT]) == 0) {
+      addr = balloc(ip->dev);
+      if (addr) {
+        a[bn / NINDIRECT] = addr;
+        log_write(bp);
+      } else {
+        brelse(bp);
+        return 0;
+      }
+    }
+
+    // read indirect block in double indirect block
+    brelse(bp);
+    bn %= NINDIRECT;
+    bp = bread(ip->dev, addr);
+    a = (uint *)bp->data;
+    if ((addr = a[bn]) == 0) {
+      addr = balloc(ip->dev);
+      if (addr) {
         a[bn] = addr;
         log_write(bp);
       }
